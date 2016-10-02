de6e5ec5e09cc32cb2bc6f6c089674beedc67738
A-n-d-y-W-e-n <a409052003@gmail.com>
2016-10-02 23:02:19 +0800

    Solved the homework; all tests pass

---
+++ yaml_buddy.rb
@@ -0,0 +1,15 @@
+# Module that can be included (mixin) to take and output Yaml data
+
+require 'yaml'
+
+module YamlBuddy
+  attr_accessor :data
+
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+end
